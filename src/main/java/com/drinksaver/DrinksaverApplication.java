156d961978f7fe265dbaaf664caee07503daecc5
Alex Molnar <alex.molnar@ing.com>
2026-01-29 16:23:05 +0100

    Created empty Spring application

---
+++ src/main/java/com/drinksaver/DrinksaverApplication.java
@@ -0,0 +1,13 @@
+package com.drinksaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinksaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinksaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drinksaver/DrinksaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drinksaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinksaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
