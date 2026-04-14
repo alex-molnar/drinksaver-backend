5c6dfe08cc893c966588f45b3c163ebc3429d15d
Alex Molnar <alex.molnar@ing.com>
2026-04-14 07:42:42 +0200

    Fix response not visible because Spring cannot set fields without getters

--- src/main/java/com/drinksaver/model/db/ConsumptionType.java
+++ src/main/java/com/drinksaver/model/db/ConsumptionType.java
@@ -2,12 +2,16 @@ package com.drinksaver.model.db;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "consumption_types")
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class ConsumptionType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/drinksaver/repository/postgres/schema/ConsumptionTypesTable.java
+++ src/main/java/com/drinksaver/repository/postgres/schema/ConsumptionTypesTable.java
@@ -2,5 +2,7 @@ package com.drinksaver.repository.postgres.schema;
 
 import com.drinksaver.model.db.ConsumptionType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConsumptionTypesTable extends JpaRepository<ConsumptionType, Integer> {}
