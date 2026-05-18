7cb494d91eff34143b28918f2638b1e24f9c7147
Alex Molnar <alex.molnar@ing.com>
2026-05-18 11:08:46 +0200

    Added DrinkKey missing class

---
+++ src/main/java/com/drinksaver/service/model/DrinkKey.java
@@ -0,0 +1,92 @@
+package com.drinksaver.service.model;
+
+import com.drinksaver.model.db.Recommendation;
+import com.drinksaver.model.db.SavedBeer;
+import com.drinksaver.model.db.SavedDrink;
+import com.drinksaver.model.db.TemporaryRecommendation;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public record DrinkKey(
+        Integer alcoholTypeId,
+        Integer alcoholSubtypeId,
+        Integer alcoholVolumeId,
+        Integer brandId,
+        Integer beerFlavourId,
+        Integer consumptionTypeId,
+        Optional<String> name
+) {
+    public Recommendation toRecommendation(UUID userId) {
+        Recommendation recommendation = new Recommendation();
+        recommendation.setUserId(userId);
+        recommendation.setName(name.orElse("Couldn't generate name"));
+        recommendation.setAlcoholTypeId(alcoholTypeId);
+        recommendation.setAlcoholSubtypeId(alcoholSubtypeId);
+        recommendation.setAlcoholVolumeId(alcoholVolumeId);
+        recommendation.setBrandId(brandId);
+        recommendation.setBeerFlavourId(beerFlavourId);
+        recommendation.setConsumptionTypeId(consumptionTypeId);
+        return recommendation;
+    }
+
+    public DrinkKey withName(String name) {
+        return new DrinkKey(
+                alcoholTypeId,
+                alcoholSubtypeId,
+                alcoholVolumeId,
+                brandId,
+                beerFlavourId,
+                consumptionTypeId,
+                Optional.of(name)
+        );
+    }
+
+    public static DrinkKey of(SavedDrink drink) {
+        return new DrinkKey(
+                drink.getAlcoholTypeId(),
+                drink.getAlcoholSubtypeId(),
+                drink.getAlcoholVolumeId(),
+                null,
+                null,
+                null,
+                Optional.empty()
+        );
+    }
+
+    public static DrinkKey of(SavedBeer beer) {
+        return new DrinkKey(
+                beer.getAlcoholTypeId(),
+                beer.getAlcoholSubtypeId(),
+                beer.getAlcoholVolumeId(),
+                beer.getBrandId(),
+                beer.getBeerFlavourId(),
+                beer.getConsumptionTypeId(),
+                Optional.empty()
+        );
+    }
+
+    public static DrinkKey of(Recommendation recommendation) {
+        return new DrinkKey(
+                recommendation.getAlcoholTypeId(),
+                recommendation.getAlcoholSubtypeId(),
+                recommendation.getAlcoholVolumeId(),
+                recommendation.getBrandId(),
+                recommendation.getBeerFlavourId(),
+                recommendation.getConsumptionTypeId(),
+                Optional.ofNullable(recommendation.getName())
+        );
+    }
+
+    public static DrinkKey of(TemporaryRecommendation recommendation) {
+        return new DrinkKey(
+                recommendation.getAlcoholTypeId(),
+                recommendation.getAlcoholSubtypeId(),
+                recommendation.getAlcoholVolumeId(),
+                recommendation.getBrandId(),
+                recommendation.getBeerFlavourId(),
+                recommendation.getConsumptionTypeId(),
+                Optional.ofNullable(recommendation.getName())
+        );
+    }
+}
